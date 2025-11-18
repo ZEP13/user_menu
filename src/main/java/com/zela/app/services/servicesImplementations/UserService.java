615c09f74cab7604bc2a411b20dacb2d21dcca69
ZEP13 <zephyr.lathuy.zpl@gmail.com>
2025-11-18 15:19:09 +0100

    user service and erroruser

--- src/main/java/com/zela/app/services/servicesImplementations/UserService.java
+++ src/main/java/com/zela/app/services/servicesImplementations/UserService.java
@@ -1,5 +1,9 @@
 package com.zela.app.services.servicesImplementations;
 
+import java.util.List;
+
+import com.zela.app.exceptions.UserException;
+import com.zela.app.models.User;
 import com.zela.app.repositories.JdbcImplementation.JdbcUserRepository;
 import com.zela.app.services.servicesInterfaces.UserServiceInterface;
 
@@ -9,6 +13,70 @@ public class UserService implements UserServiceInterface {
 
     public UserService(JdbcUserRepository repository) {
         this.repository = repository;
+    }
+
+    public User save(User user) {
+        validateForCreate(user);
+        try {
+            return repository.save(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User update(User user) {
+        validateForUpdate(user);
+        try {
+            return repository.update(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteById(int id) {
+        validateForDelete(id);
+        try {
+            return repository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<User> allUser() {
+        try {
+            return repository.allUser();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateForCreate(User user) {
+        if (user == null)
+            throw new IllegalArgumentException("user is null");
+
+        if (user.getNom() == null || user.getNom().isBlank())
+            throw new UserException("titre is required");
+
+        if (user.getPrenom() == null || user.getPrenom().isBlank())
+            throw new UserException("contenu is required");
+
+        if (user.getId() <= 0)
+            throw new UserException("auteur_id is required and must be positive");
+    }
+
+    private void validateForUpdate(User a) {
+        if (a == null)
+            throw new IllegalArgumentException("user is null");
+        if (a.getId() <= 0)
+            throw new UserException("id_user is invalid");
+        validateForCreate(a);
+    }
+
+    private void validateForDelete(Integer id) {
+        if (id == null)
+            throw new IllegalArgumentException("id is null");
+        if (id <= 0)
+            throw new UserException("id is invalid");
     }
 
 }
--- src/test/java/com/zela/app/AppTest.java
+++ src/test/java/com/zela/app/AppTest.java
@@ -1,19 +1,1 @@
-package com.zela.app;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-}
